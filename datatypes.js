99d3f7c8f2975040644644d1e2daf66645fb764a
Aman Malik <132246284+amanXmalik@users.noreply.github.com>
2023-09-26 18:17:52 +0000

    string methods

--- datatypes.js
+++ datatypes.js
@@ -1,4 +1,12 @@
 let a = 1
 console.log(typeof a);
 let b = "abcdef"
-console.log(typeof b)
+console.log(typeof b);
+
+const balance = new Number (100)
+console.log(balance.toString().length);
+
+const otherNum = 123.8966
+console.log(otherNum.toPrecision(3));
+
+
