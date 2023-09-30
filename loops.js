f28aa4eef261e7ee9de45269bfb2b7df7ad885dd
Aman Malik <132246284+amanXmalik@users.noreply.github.com>
2023-09-30 11:42:35 +0000

    loops

---
+++ loops.js
@@ -0,0 +1,20 @@
+let a;
+for(a=1;a<6;a++){
+    console.log(a);
+   
+}
+let b =3
+if(b===3){
+    console.log(`value of a is ${b}`);
+}
+
+let c = 11
+
+while(c>0){
+    console.log(c);
+    c = c - 1;
+}
+
+do{
+    console.log(`do while executes ${c} times`);
+} while(c>1)
